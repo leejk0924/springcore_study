16790732b35db52a7dc04fb7719cc3d7e91fed96
leejk0924 <akdirdl@gmail.com>
2022-05-31 23:40:12 +0900

    lecture: 스프링 핵심원리 이해1 - 예제만들기(주문과 할인 도메인 실행과 테스트)

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -8,10 +8,11 @@ public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000; // 1000원 할인
 
     @Override
-    public int discount(Member member) {
+    public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP)
             return discountFixAmount;
         else
             return 0;
     }
+
 }
