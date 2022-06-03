7541e9efe9fb71f521cea3c7e0e045c7b93eb962
leejk0924 <akdirdl@gmail.com>
2022-06-03 23:02:12 +0900

    lecture: 스프링 핵심 원리 이해2 - 객체지향 원리 적용(Appconfig 리팩터링)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,11 +13,20 @@ public class AppConfig {
 
     // 생성자 주입 : 생성자를 통해서 객체를 주입
    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // 메서드 명을 통해서 역할을 유추할 수 있다.
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 
