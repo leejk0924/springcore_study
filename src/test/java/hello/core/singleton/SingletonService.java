3829fc4594f9adf190647a11530be68e22c3cdf2
leejk0924 <akdirdl@gmail.com>
2022-06-08 19:38:03 +0900

    lecture: 싱글톤 컨테이너와 (싱글톤 패턴)

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,22 @@
+package hello.core.singleton;
+
+public class SingletonService {
+    private static final SingletonService INSTANCE = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return INSTANCE;
+    }
+
+    private SingletonService() {
+
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+    public static void main(String[] args) {
+        SingletonService singletonService = new SingletonService();
+
+    }
+
+}
