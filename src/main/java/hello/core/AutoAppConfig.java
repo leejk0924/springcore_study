6ac3428ea47b28f9a215bd6becb1c361feb2b0e3
leejk0924 <akdirdl@gmail.com>
2022-06-09 16:47:09 +0900

    lecture: 싱글톤 컨테이너와 (탐색 위치와 기본 스캔 대상)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 @Configuration
 @ComponentScan(         // @Component 에너테이션이 붙은 클래스를 자동으로 컨테이너에 등록
         // ComponentScan 하여 스프링빈에 자동등록하는데 그중에 필터링할 클래스 지정
+        basePackages="hello.core.member",       // 탐색할 패키지의 시작 위치 지정 (hello.core.member 패키지부터 시작해서 하위패키지 검색)
+        basePackageClasses = AutoAppConfig.class,       // AutoAppConfig 클래스의 패키지부터 검색
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)     // Configuration 붙은 에너테이션 제외
 )
 
