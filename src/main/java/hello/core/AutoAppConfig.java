da371b7be8733f6dbceb03f1fc8aca4b0bfbfd1c
leejk0924 <akdirdl@gmail.com>
2022-06-09 21:18:58 +0900

    lecture: 싱글톤 컨테이너와 (중복 등록과 충돌)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -16,6 +19,5 @@ import org.springframework.stereotype.Component;
 
 
 public class AutoAppConfig {
-
 
 }
