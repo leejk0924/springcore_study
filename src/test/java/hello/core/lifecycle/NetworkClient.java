fe07b4b170ee0f1a730704d00a5c6d04d21e9646
Jung-Kwon Lee <akdirdl@gmail.com>
2022-06-14 17:58:30 +0900

    lecture: 빈 생명주기 콜백 (인터페이스 InitializingBean, DisposableBean)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean , DisposableBean {
     private String url;
 
     public NetworkClient() {
-        System.out.println("생성자 호출, url =" + url);
-        connect();
-        call("초기화 연결 메시지");
+        System.out.println("생성자 호출, url = " + url);
+
     }
 
     public void setUrl(String url) {
@@ -28,4 +30,18 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
+    // 초기화 끝나고 실행
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 소멸전 실행
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
 }
