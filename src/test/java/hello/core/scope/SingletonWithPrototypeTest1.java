d5cc3d20d3bd56cbc6d101cdeb5fcbfa424a2403
Jung-Kwon Lee <akdirdl@gmail.com>
2022-06-14 22:07:12 +0900

    lecture: 빈 스코프(프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결)

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,15 @@ package hello.core.scope;
 
 import ch.qos.logback.core.net.server.Client;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,39 +39,22 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean1.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;  // 생성시점에 주입   x01의 프로토타입
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;    // 편의상 필드 주입 했으나 생성자 주입 하는게 좋음
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        // 원래는 ObjectFactory 였으나 편의기능을 추가하여 ObjectProvider 을 만들었음
 
         public int logic() {
-
-            prototypeBean.addCount();
-            int count = prototypeBean.getCount();
-            return count;
-        }
-    }
-
-    @Scope("singleton")
-    static class ClientBean2 {
-        private final PrototypeBean prototypeBean;  // 생성시점에 주입   x02의 프로토타입
-
-        @Autowired
-        public ClientBean2(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic() {
-
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();       // getObject 를 호출하면 그때 스프링 컨테이너에서 프로토타입 빈을 찾아서 반환 (DL)
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
