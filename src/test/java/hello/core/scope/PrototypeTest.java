7369675921cb4202fe81857a4b9d7376a21d6fdd
Jung-Kwon Lee <akdirdl@gmail.com>
2022-06-14 21:28:44 +0900

    lecture: 빈 스코프(프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점)

--- src/test/java/hello/core/scope/PrototypeTest.java
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -22,7 +22,7 @@ public class PrototypeTest {
         System.out.println("prototypeBean2 = " + prototypeBean2);
         assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
 
-        
+
         ac.close();
     }
 
