b6771367dbb8ab81c01b17527af843e8ba2d0b41
Jung-Kwon Lee <akdirdl@gmail.com>
2022-06-14 23:10:14 +0900

    lecture: 빈 스코프(스코프와 프록시)

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,14 +1,18 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.UUID;
 
+// proxyMod=ScopedProxyMode.TARGET_CLASS 가 핵심 (적용대상이 인터페이스면 INTERFACES 선택)
+// MyLogger의 가짜 프록시 클래스를 만들어두고 HTTP request 와 상관없이 가짜 프록시 클래스를 다른 빈에 미리 주입해 둘 수 있다.
+
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode= ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
     private String uuid;
     private String requestURL;
