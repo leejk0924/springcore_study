807790bca5f7ffea5051ee1cffbb433547ef15a2
Jung-Kwon Lee <akdirdl@gmail.com>
2022-09-11 23:03:42 +0900

    rafactor: change

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -2,12 +2,17 @@ package hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoreApplication.class, args);
+
+
 	}
 
 }
